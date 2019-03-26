68486681e0c7db1083097d5a5a621dad7c989287
OKOJ <aajmd@hotmail.com>
2019-03-25 18:21:59 -0600

    added space

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -294,13 +294,13 @@ function toggleButton() {
 }
 
 // Checks that the Firebase SDK has been correctly setup and configured.
-function checkSetup() {
+/*function checkSetup() {
   if (!window.firebase || !(firebase.app instanceof Function) || !firebase.app().options) {
     window.alert('You have not configured and imported the Firebase SDK. ' +
         'Make sure you go through the codelab setup instructions and make ' +
         'sure you are running the codelab using `firebase serve`');
   }
-}
+}*/
 
 // Checks that Firebase has been imported.
 checkSetup();
